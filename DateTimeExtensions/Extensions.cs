22c787c6e969224beab2cb30031f949489a1e4a4
Glen Wilkin <glen@luthansolutions.com>
2024-01-20 15:29:27 +0000

    ToUniversalTime()

--- DateTimeExtensions/Extensions.cs
+++ DateTimeExtensions/Extensions.cs
@@ -2,22 +2,24 @@
 
 public static class Extensions
 {
-    public static bool IsBefore(this DateTime dateTime, DateTime value) => 
-        dateTime < value;
+    public static bool IsBefore(this DateTime dateTime, DateTime value) =>
+        dateTime.ToUniversalTime() < value.ToUniversalTime();
 
-    public static bool IsAfter(this DateTime dateTime, DateTime value) => 
-        dateTime > value;
+    public static bool IsAfter(this DateTime dateTime, DateTime value) =>
+        dateTime.ToUniversalTime() > value.ToUniversalTime();
 
-    public static bool IsBeforeOrEqual(this DateTime dateTime, DateTime value) => 
-        dateTime <= value;
+    public static bool IsBeforeOrEqual(this DateTime dateTime, DateTime value) =>
+        dateTime.ToUniversalTime() <= value.ToUniversalTime();
 
-    public static bool IsAfterOrEqual(this DateTime dateTime, DateTime value) => 
-        dateTime >= value;
+    public static bool IsAfterOrEqual(this DateTime dateTime, DateTime value) =>
+        dateTime.ToUniversalTime() >= value.ToUniversalTime();
 
     public static bool IsBetweenInclusive(this DateTime dateTime, DateTime firstDateTime, DateTime lastDateTime) =>
-        dateTime >= firstDateTime && dateTime <= lastDateTime;
+        dateTime.ToUniversalTime() >= firstDateTime.ToUniversalTime() &&
+        dateTime.ToUniversalTime() <= lastDateTime.ToUniversalTime();
 
     public static bool IsBetweenExclusive(this DateTime dateTime, DateTime firstDateTime, DateTime lastDateTime) =>
-        dateTime > firstDateTime && dateTime < lastDateTime;
+        dateTime.ToUniversalTime() > firstDateTime.ToUniversalTime() &&
+        dateTime.ToUniversalTime() < lastDateTime.ToUniversalTime();
 
 }
